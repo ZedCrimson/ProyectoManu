e5cb7ba99394c5b87b9abd1f765131da177544a6
unknown <sgalanbrenes@gmail.com>
2024-11-21 11:27:55 +0100

    Inicio Proyecto

---
+++ Index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Casino </title>
+</head>
+
+<body>
+
+    <div class="login">
+	<div class="container">
+		<div class="logo">Gimnasio No+Panza</div>
+		<form class="form" method="POST" action="login.php">
+			<input type="usuario" class="input" placeholder="Usuario" name="usuario" required>
+			<input type="password" class="input" placeholder="Contraseña" name="contraseña" required>
+			<button type="submit" class="button">Iniciar sesión</button>
+		</form>
+		
+        </div>
+	</div>
+</body>
+
+</html>
